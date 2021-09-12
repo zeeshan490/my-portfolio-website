0748439db9712b5d4cbfcd1aba704688cf8b1be6
zeeshan490 <79791483+zeeshan490@users.noreply.github.com>
2021-09-12 18:01:25 +0530

    portfolio section complete and working

--- script.js
+++ script.js
@@ -14,3 +14,45 @@ tabsContainer.addEventListener('click' , (e) =>{
     }
 })
 
+/* portfolio item details */
+
+document.addEventListener("click" , (e) =>{
+    if(e.target.classList.contains("view-project-btn")){
+        togglePortfolioPopup();
+        document.querySelector(".portfolio-popup").scrollTo(0,0);
+        portfolioItemDetails(e.target.parentElement);
+    }
+})
+function togglePortfolioPopup(){
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+
+document.querySelector(".pp-close").addEventListener("click" , togglePortfolioPopup);
+
+
+//hide popup by clicking anywhere outside it
+
+document.addEventListener("click" , (e)=>{
+    if(e.target.classList.contains("pp-inner")){
+        togglePortfolioPopup();
+    }
+})
+
+
+function portfolioItemDetails(portfolioItem){
+    document.querySelector(".pp-thumbnail img").src =
+    portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+
+
+    document.querySelector(".pp-header h3").innerHTML =
+    portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+
+    document.querySelector(".pp-body").innerHTML =
+    portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+
+
+    
+}
